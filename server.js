f6b696955786b6576c53bcce09fc1821ec1f0e7e
itashdv <itashcb@gmail.com>
2020-06-22 10:34:35 +0700

    First commit pushing to server for testing connection.

---
+++ server.js
@@ -0,0 +1,16 @@
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', (req, res) => {
+	res.send('<h2 style="text-align: center;">Welcome to Autobook sockets!</h2>');
+});
+
+io.on('connection', socket => {
+	socket.emit('news', { hello: 'world' });
+	socket.on('my other event', data => {
+		console.log(data);
+	});
+});
