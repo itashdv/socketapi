f82d736675395fe491c756a8fbd0c27267a7f895
itashdv <itashcb@gmail.com>
2020-06-22 11:57:43 +0700

    Replaced https with http for debugging. 7

--- server.js
+++ server.js
@@ -2,15 +2,25 @@ var fs = require('fs');
 var app = require('express')();
 var https = require('https');
 
-var server = https.createServer({
-  key: fs.readFileSync('/etc/letsencrypt/live/autobook.world/privkey.pem', 'utf8'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/autobook.world/cert.pem', 'utf8'),
-  ca: fs.readFileSync('/etc/letsencrypt/live/autobook.world/chain.pem', 'utf8'),
-  requestCert: false,
-  rejectUnauthorized: false
+// var server = https.createServer({
+//   key: fs.readFileSync('/etc/letsencrypt/live/autobook.world/privkey.pem', 'utf8'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/autobook.world/cert.pem', 'utf8'),
+//   ca: fs.readFileSync('/etc/letsencrypt/live/autobook.world/chain.pem', 'utf8'),
+//   requestCert: false,
+//   rejectUnauthorized: false
+// }, app);
+
+const server = https.createServer({
+	key: fs.readFileSync('/etc/letsencrypt/live/autobook.world/privkey.pem'),
+	cert: fs.readFileSync('/etc/letsencrypt/live/autobook.world/cert.pem'),
+	ca: fs.readFileSync('/etc/letsencrypt/live/autobook.world/chain.pem'),
+	requestCert: false,
+	rejectUnauthorized: false
 }, app);
 
-server.listen(80);
+server.listen(3000);
+
+// server.listen(80);
 
 var io = require('socket.io').listen(server);
 
