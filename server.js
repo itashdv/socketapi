b1c1e81b4ceeefc36a8d1d7fab6b19fb7fb0b825
itashdv <itashcb@gmail.com>
2020-06-22 11:43:51 +0700

    Replaced https with http for debugging. 3

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/autobook.world/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/autobook.world/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/autobook.world/chain.pem', 'utf8');
-const credentials = { key: privateKey, cert: certificate, ca: ca };
+// const fs = require('fs');
+// // Certificate
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/autobook.world/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/autobook.world/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/autobook.world/chain.pem', 'utf8');
+// const credentials = { key: privateKey, cert: certificate, ca: ca };
 const app = require('express')();
-const server = require('https').Server(credentials, app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
 server.listen(80);
